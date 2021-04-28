4d687cf446c29b4fae7755b72560132ce531b6c1
Pol Dellaiera <pol.dellaiera@protonmail.com>
2021-04-28 07:18:51 +0200

    cs: Autofix.

--- src/Foo/Bundle/TestBundle/src/Tests/Fixtures/Context/DemoContext.php
+++ src/Foo/Bundle/TestBundle/src/Tests/Fixtures/Context/DemoContext.php
@@ -1,15 +1,26 @@
 <?php
 
-# tests/Behat/DemoContext.php
+/**
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+// tests/Behat/DemoContext.php
 
 namespace Foo\TestBundle\Tests\Fixtures\Context;
+
 // If using Symfony 3, use namespace "Tests\Behat" instead
 
 use Behat\Behat\Context\Context;
+use RuntimeException;
 
 final class DemoContext implements Context
 {
-    /** @var string */
+    /**
+     * @var string
+     */
     private $environment;
 
     public function __construct(string $environment)
@@ -23,7 +34,7 @@ final class DemoContext implements Context
     public function kernelEnvironmentShouldBe(string $expected): void
     {
         if ($this->environment !== $expected) {
-            throw new \RuntimeException();
+            throw new RuntimeException();
         }
     }
 }
